30dbc201eee3bc25ae9e266cabfe256fab8e9afd
jumashafara <jumashafara0@gmail.com>
2023-02-27 14:50:01 +0300

    initialized project with templates and basic urls

---
+++ predictor/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def uploadImage(request):
+    pass
+
+
+def getPredictions(request):
+    pass
+
+
+def deleteImage(request):
+    pass
