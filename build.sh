89d17845dc7817fc9d3aa945c9b6ccb9b471c8ab
jumashafara <jumashafara0@gmail.com>
2023-07-07 14:32:00 +0300

    removed whitenoise from build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,8 @@
 set -o errexit
 
 pip install --upgrade pip
-pip install 'whitenoise[brotli]'
 pip install --upgrade Pillow
 
-echo "Installing the latest version of poetry..."
-pip install poetry==1.2.0
 rm poetry.lock
 poetry lock
 poetry install
