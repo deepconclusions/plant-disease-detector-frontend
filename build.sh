b3127d98dddf2a627c3b5656f3fbb3f86add049a
jumashafara <jumashafara0@gmail.com>
2023-07-07 10:27:13 +0300

    Added build.sh and adjusted settings for render

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install --upgrade pip
+
+pip install --upgrade Pillow
+
+echo "Installing the latest version of poetry..."
+pip install poetry==1.2.0
+rm poetry.lock
+poetry lock
+poetry install
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+pip install --force-reinstall -U setuptools
