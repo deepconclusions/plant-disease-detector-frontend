e8b6a45787073201fc3edc99fb33073de8d668dc
jumashafara <jumashafara0@gmail.com>
2023-08-02 10:14:06 +0300

    setup settings for pythonanywhere

--- plants/urls.py
+++ plants/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from . import views
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = "plants"
 
@@ -25,10 +26,13 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
     path("about", views.about, name="about"),
-    path("pricing", views.pricing, name = "pricing"),
-    path("support", views.support, name= "support"),
-    path("careers", views.careers, name= "careers"),
-    path("videos", views.videos, name = "videos"),
+    path("pricing", views.pricing, name="pricing"),
+    path("support", views.support, name="support"),
+    path("careers", views.careers, name="careers"),
+    path("videos", views.videos, name="videos"),
     path('accounts/', include('accounts.urls')),
     path('predictor/', include('predictor.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
